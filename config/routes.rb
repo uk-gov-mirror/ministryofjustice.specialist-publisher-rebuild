2a8f74fd28819978aa687cefeca3c42a83b27514
Rosa Fox <rosafox89@gmail.com>
2016-09-13 11:54:27 +0000

    Redirects '/'
    - Now that manuals code has been removed `/manuals` can no longer be the root url
    - The root will be the finder index page of the organisation a user is logged in with
    
    i.e
    - A cma-case editor's root url will be `/cma-cases`
    - A gds-editor root url will be `/aaib-reports`. This was chosen as it is top of the drop-down alphabetically.
    - If the user has access to multiple formats then one of their format index pages will be the root url.
    
    `/error`:
    - This route has been created in the very unlikely case that a user logs on with an `organisation_content_id` that does not match up to an organisation recognised by this application

--- config/routes.rb
+++ config/routes.rb
@@ -2,20 +2,11 @@ Rails.application.routes.draw do
   get '/healthcheck', to: proc { [200, {}, ['OK']] }
   get '/rebuild-healthcheck', to: proc { [200, {}, ['OK']] }
   post '/preview', to: 'govspeak#preview'
+  get '/error', to: 'passthrough#error'
 
   mount GovukAdminTemplate::Engine, at: "/style-guide"
 
-  resources :manuals, param: :content_id, except: :destroy do
-    post :unpublish, on: :member
-    post :publish, on: :member
-
-    resources :sections, param: :content_id, except: :destroy, controller: "manual_sections" do
-      resources :attachments, controller: 'manual_sections_attachments', param: :attachment_content_id, only: [:new, :create, :edit, :update]
-
-      get :reorder, on: :collection
-      post :update_order, on: :collection
-    end
-  end
+  resources :passthrough, only: [:index]
 
   resources :documents, path: "/:document_type_slug", param: :content_id, except: :destroy do
     resources :attachments, param: :attachment_content_id, only: [:new, :create, :edit, :update]
@@ -24,5 +15,5 @@ Rails.application.routes.draw do
     post :publish, on: :member
   end
 
-  root to: redirect("/manuals")
+  root to: 'passthrough#index'
 end
